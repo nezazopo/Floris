3da886cf0428845b791fe6b029a7173f44031e7b
nezazp <neza.zo.po@gmail.com>
2026-03-29 21:37:23 +0200

    izberi odgovor - nedokoknčano

---
+++ igra/scripts/panel_izberi_dn.gd
@@ -0,0 +1,11 @@
+extends Panel
+#gleda, uporabnikovo odločitev glede rastline
+signal odgovor_izbran(odgovor: String)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Da"):
+		emit_signal("odgovor_izbran", "da")
+	if Input.is_action_just_pressed("Ne"):
+		emit_signal("odgovor_izbran", "Da")
+		
--- igra/scripts/rastlina.gd
+++ igra/scripts/rastlina.gd
@@ -7,39 +7,66 @@ class_name Rastlina
 
 @onready var aspect_ratio_container: AspectRatioContainer = %AspectRatioContainer
 @onready var game_manager = %GameManager
-# Called when the node enters the scene tree for the first time.
 @onready var panel_izberi_dn: Panel = %PanelIzberiDN
 
 func _ready():
+	#pripravim video
 	if(aspect_ratio_container != null):
 		aspect_ratio_container.visible = false
+	#pripravim panel za izbiro
 	if(panel_izberi_dn != null):
 		panel_izberi_dn.visible = false;
-	add_to_group("rastline")
+	add_to_group("rastline") #rastlino dodam v "rastline", array, definiran v gamemanager
 	$Area3D.body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body: Node3D):
 	if body is CharacterBody3D:  #ce je igralec vstopil v okolico rastline
 		#pritisni enter da si ogledaš rastlino
-		aspect_ratio_container.visible = true
-		panel_izberi_dn.visible = true;
-		#if(self == poisci && action.justpressed("da")):
-		#pravilno si našel
-		#
-		if self == (game_manager.poisciRastlino):
+		aspect_ratio_container.visible = true #video
+		panel_izberi_dn.visible = true;	#panel da/než
+		
+		var odgovor = await panel_izberi_dn.odgovor_izbran
+		#PRAVILEN ODGOVOR
+		if(self == game_manager.poisciRastlino && odgovor == "da"):
 			print("pravilno si našel " + ime +"!")
 			game_manager.pristej()
 			print("st tock: ", game_manager.tocke)
 			game_manager.naslednjaRastlina()
 			if(!game_manager.konec):
 				print("sedaj poisci " + game_manager.poisciRastlino.ime)
-		else:
+		
+		if(self != game_manager.poisciRastlino &&  odgovor == "ne"):
+			print("Res je, to ni " + ime +"!")
+		
+		#NAPAČEN: iskana rastlina, odg NE
+		if(self == game_manager.poisciRastlino &&  odgovor == "ne"):
+			print("napačen odgovor! -1 točka")
+		#NAPAČEN: napačna rastlina odg DA
+		if(self != game_manager.poisciRastlino && odgovor == "da"):
+			print("napačen odgovor! -", game_manager.tocke, "točke")
 			if strupenost == 2: #ce je smrtonosna
 				game_manager.umre()
 			else:
 				game_manager.odstej(1 + strupenost);
-			if game_manager.tocke < 0:
-				print("Ni ti uspelo :(") 
+			if game_manager.tocke <= 0:
+				print("Ni ti uspelo :(")
+		panel_izberi_dn.visible = false
+		aspect_ratio_container.visible = false
+			
+		#if self == (game_manager.poisciRastlino):
+		#	print("pravilno si našel " + ime +"!")
+		#	game_manager.pristej()
+		#	print("st tock: ", game_manager.tocke)
+		#	game_manager.naslednjaRastlina()
+		#	if(!game_manager.konec):
+		#		print("sedaj poisci " + game_manager.poisciRastlino.ime)
+		#else:
+		#	if strupenost == 2: #ce je smrtonosna
+		#		game_manager.umre()
+		#	else:
+		#		game_manager.odstej(1 + strupenost);
+		#	if game_manager.tocke < 0:
+		#		print("Ni ti uspelo :(") 
 
-func vrniSliko():
+func _process(delta: float) -> void:
 	pass
